692e6650372da4be5918d5ec48c5c140ae805b85
Documenter.jl <documenter@juliadocs.github.io>
2024-09-18 17:49:32 +0000

    build based on 95e35bb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MutableOperations","category":"page"},{"location":"#MutableOperations","page":"Home","title":"MutableOperations","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MutableOperations.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MutableOperations]","category":"page"}]
+}
